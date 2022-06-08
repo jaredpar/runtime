1d44b2f51f445e207862c085376d83372ef5360a
Jared Parsons <jaredpparsons@gmail.com>
2022-06-08 15:35:25 -0700

    Infra

--- src/libraries/System.Text.RegularExpressions/tests/UnitTests/UpgradeToRegexGeneratorAnalyzerTests.cs
+++ src/libraries/System.Text.RegularExpressions/tests/UnitTests/UpgradeToRegexGeneratorAnalyzerTests.cs
@@ -1,6 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-
+/*
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -770,3 +770,4 @@ partial class Program
         #endregion Test helpers
     }
 }
+*/
